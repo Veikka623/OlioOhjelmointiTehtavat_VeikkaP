c2ee4cfde63e368a56725ac48237bd15148139aa
Veikka623 <veikka.poyhtari@gmail.com>
2025-01-08 11:21:09 +0200

    testiä
    testi

---
+++ testi/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Test" << endl;
+    cout << "Test2" << endl;
+    cout << "Test3" << endl;
+    return 0;
+}
